cfa97645cb232ac2a17781f4278b679ebf45aee2
Elivelton Silva de Jesus Santos <elivelton90@hotmail.com>
2012-09-29 17:43:14 -0300

    Autenticação de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Sigma::Application.routes.draw do
+  devise_for :users
+
   resources :companies
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Sigma::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'companies#index'
 
   # See how all your routes lay out with "rake routes"
 
