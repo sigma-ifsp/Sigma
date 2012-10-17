81f56c392601fbe80393c2efe6d87bd4af94c02f
Philip Sampaio <philip.sampaio@gmail.com>
2012-10-17 00:21:19 -0300

    Adicionando dados para popular base de dados.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
-# Roles
-Role.create([
-            { name: 'root', description: 'System admin'},
-            { name: 'client', description: 'Companies Client'},
-            { name: 'admin', description: 'Company admin' },
-            { name: 'employee', description: 'Company employee'}
-])
+# Roles - Important
+
+root_role = Role.create(name: 'root', description: 'System admin')
+client_role = Role.create(name: 'client', description: 'Companies Client')
+admin_role = Role.create(name: 'admin', description: 'Company admin')
+cashier_role = Role.create(name: 'employee', description: 'Company employee')
 
 PromotionCategory.create([{ name: 'points'}, {name: 'value'}])
+
+# Sample Companies
+company = Company.create(name: 'Mit su yan', cnpj: '11.284.880/0001-91', email: 'mit@email.com')
+
+# Admin user
+admin_user = User.new(username: 'maria', email: 'admin@mitsuyan.com.br', password: '123456', password_confirmation: '123456', role: admin_role)
+admin = Employee.create(name: 'Maria', company: company, user: admin_user)
+
+# Cashier
+cashier_user = User.new(username: 'ivete', email: 'cashier@mitsuyan.com.br', password: '123456', password_confirmation: '123456', role: cashier_role)
+cashier = Employee.create(name: 'Ivete', company: company, user: cashier_user)
