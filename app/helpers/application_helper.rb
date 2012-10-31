91bbbfa94cabfdd1b2f8b860d719a7d8ac3f71a1
Philip Sampaio <philip.sampaio@gmail.com>
2012-10-31 03:17:52 -0200

    Reposicionando menus e mudando mensagens de tradução

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,28 +12,29 @@ module ApplicationHelper
              []
            end
     content_tag :ul,
-      menu.map {|link| content_tag(:li, link) }.join("\n").html_safe
+      menu.map {|link| content_tag(:li, link) }.join("\n").html_safe, id: 'menu-bar'
   end
 
   def menu_for_client(client)
     [
-      link_to(I18n.t('sigma.points'), {:controller => '/clients', :id => client.id, :action => 'points'}),
+      link_to(I18n.t('sigma.points.title'), {:controller => '/clients', :id => client.id, :action => 'points'}),
       link_to(I18n.t('sigma.profile'), edit_user_registration_path(client.user_id))
     ]
   end
 
   def menu_for_cashier(cashier)
     [
-      link_to(I18n.t('sigma.points'), new_point_path),
-      link_to(I18n.t('sigma.promotions'), promotions_path)
+      link_to(I18n.t('sigma.points.title'), new_point_path),
+      link_to(I18n.t('sigma.promotions.title'), promotions_path)
     ]
   end
 
   def menu_for_admin(admin)
     [
-      link_to(I18n.t('sigma.points'), new_point_path),
-      link_to(I18n.t('sigma.promotions'), promotions_path),
-      link_to(I18n.t('sigma.company'), company_path(admin.company_id))
+      link_to(I18n.t('sigma.points.title'), new_point_path),
+      link_to(I18n.t('sigma.promotions.title'), promotions_path),
+      link_to(I18n.t('sigma.company'), company_path(admin.company_id)),
+      link_to(I18n.t('sigma.reports.exchanges.label'), exchanges_path)
     ]
   end
 
