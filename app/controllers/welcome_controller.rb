737dc46ecce89a7f2f873aedbd2a27ee8428d0bf
Philip Sampaio <philip.sampaio@gmail.com>
2012-10-31 03:14:32 -0200

    Redirecionando usuário Client para pontos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      if current_user.client?
+        redirect_to :controller => '/clients', :action => 'points', :id => current_user.client.id
+      end
+      return
+    end
   end
 end
