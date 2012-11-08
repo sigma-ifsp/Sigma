8922e8d2ae609dcf56669afe0334dee80da2191d
Jade Bueno de Lima <jadeb.lima@gmail.com>
2012-11-07 22:34:03 -0200

    Inicio de reformulação do layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    redirect_to user_session_path and return unless current_user
     if current_user
       if current_user.client?
         redirect_to :controller => '/clients', :action => 'points', :id => current_user.client.id
+      elsif current_user.admin? || current_user.cashier?
+        redirect_to new_point_path
       end
       return
     end
