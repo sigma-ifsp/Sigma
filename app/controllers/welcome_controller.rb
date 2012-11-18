bae988ea22874059610d0aaa15642e9877e5c75c
Philip Sampaio <philip.sampaio@gmail.com>
2012-11-18 17:07:09 -0200

    Redirecionando para companies_path quando root

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,15 @@
 class WelcomeController < ApplicationController
   def index
-   if current_user
-     if current_user.client?
-       redirect_to :controller => '/clients', :action => 'points', :id => current_user.client.id
-     elsif current_user.admin? || current_user.cashier?
-       redirect_to new_point_path
-     end
-     return
-   end
+    if current_user
+      if current_user.client?
+        redirect_to :controller => '/clients', :action => 'points', :id => current_user.client.id
+      elsif current_user.admin? || current_user.cashier?
+        redirect_to new_point_path
+      elsif current_user.root?
+        redirect_to companies_path
+      end
+      return
+    end
   end
 
   def about_us
